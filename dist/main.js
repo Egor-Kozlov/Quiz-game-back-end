4520f63c29c5a4f58dd94b5363e6200ce7fa36d6
Egor-Kozlov <kozlov1998.2010@gmail.com>
2023-07-06 15:32:16 +0200

    feat: connect mongoDb

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://egorSkozlov:19982010xbox@cluster0.lxveyvq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
